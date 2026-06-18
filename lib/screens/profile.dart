4c6dbfa121148d2114e2357c34609b872cd33c54
LeoVier <leoweitlaner@gmail.com>
2026-06-18 19:34:16 +0200

    Implement Profile screen with user details and navigation to Settings

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,10 +1,85 @@
 import 'package:flutter/material.dart';
 
+// packages
+import 'package:firebase_auth/firebase_auth.dart';
+
+// screens
+import 'package:orbit/screens/settings.dart';
+
 class Profile extends StatelessWidget {
   const Profile({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final user = FirebaseAuth.instance.currentUser;
+    final photoUrl = user?.photoURL;
+    final displayName = user?.displayName;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Mein Profil',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              await Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Settings()),
+              );
+            },
+            icon: Icon(Icons.settings),
+          ),
+        ],
+      ),
+
+      body: Padding(
+        padding: EdgeInsetsGeometry.all(18.0),
+        child: Column(
+          children: [
+            Card(
+              elevation: 0,
+              child: Row(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(1000),
+                    child: Image.network(
+                      photoUrl ?? 'assets/icon.png',
+                      width: 60,
+                      height: 60,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+
+                  const SizedBox(width: 14),
+
+                  Column(
+                    mainAxisAlignment: .start,
+                    crossAxisAlignment: .start,
+                    children: [
+                      Text(
+                        displayName ?? '',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+
+                      const SizedBox(height: 2),
+
+                      const Text(
+                        'Test | Test | Test',
+                        style: TextStyle(fontSize: 12),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
